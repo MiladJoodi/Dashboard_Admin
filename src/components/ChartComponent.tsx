d91ce3fcb0dc211f666821f75faacd5c8f7cc15e
Milad Joodi <winbeta.com@gmail.com>
2024-05-15 02:29:12 +0330

    Update ChartComponent.tsx

--- src/components/ChartComponent.tsx
+++ src/components/ChartComponent.tsx
@@ -3,38 +3,25 @@ import Chart from "react-apexcharts"
 const ChartComponent = () => {
     
     const options = {
-        chart: {
-          type: "area",
-        },
-    
         xaxis: {
-          labels: {
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            formatter: function (value: any) {
-              const dates = [
-                "1402/01/01",
-                "1402/01/02",
-                "1402/01/03",
-                "1402/01/04",
-                "1402/01/05",
-                "1402/01/06",
-                "1402/01/07",
-                "1402/01/08",
-                "1402/01/09",
-                "1402/01/10",
-                "1402/01/11",
-              ];
-              return dates[value];
-            },
-          },
-        },
+          categories: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+        }
       };
       const series = [
         {
-          name: "فروش",
-          data: [30, 140, 85, 50, 90, 60, 70, 91, 125],
+          name: "series-1",
+          data: [30, 40, 25, 50, 49, 21, 70, 51]
         },
+        {
+          name: "series-2",
+          data: [23, 12, 54, 61, 32, 56, 81, 19]
+        },
+        {
+          name: "series-3",
+          data: [24, 20, 5, 75, 42, 79, 72, 35]
+        }
       ];
+    
 
     return (
         <div className="w-full bg-white shadow-sm rounded-xl py-4 px-3">
